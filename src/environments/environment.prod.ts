d7727d1d17b2b03cb37fbc2acdcb0ed2fa83544a
Ricardo Alvarado Aponte <ricardoalvaradoaponte@gmail.com>
2021-03-25 15:51:11 -0500

    tarjeta digital

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,26 @@
+import createNumberMask from 'text-mask-addons/dist/createNumberMask';
+
 export const environment = {
-  production: true
+  production: true,
+  keyRecaptcha: '6Lc4jf8UAAAAAP1RUJU44sDzMEv-ioRmoYMveAOy'
 };
+
+export const MASK_DOC_NUMBER = createNumberMask({
+  prefix: '',
+  includeThousandsSeparator: false,
+  allowDecimal: false,
+  allowLeadingZeroes: true
+});
+export const MASK_NUMBER = createNumberMask({
+  prefix: '',
+  includeThousandsSeparator: true,
+  thousandsSeparatorSymbol: ',',
+  allowDecimal: false
+});
+export const MASK_DECIMAL = createNumberMask({
+  prefix: '',
+  includeThousandsSeparator: true,
+  thousandsSeparatorSymbol: ',',
+  allowDecimal: true,
+  decimalSymbol: '.'
+});
